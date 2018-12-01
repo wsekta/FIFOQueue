9602b7591089472d790a00c5d3e56b16b8bce462
Wojciech Sekta <wojtek1994pl@gmail.com>
2018-12-01 22:45:01 +0100

    Project pattern

---
+++ kolejkaFIFO/FIFOQueue.cpp
@@ -0,0 +1,54 @@
+#include "FIFOQueue.h"
+
+void QFCreate( QueueFIFO* q )
+{
+	
+}
+
+
+
+//=======================================
+int QFEmpty( QueueFIFO* q )
+{
+	return 0;
+}
+
+
+
+//=======================================
+void QFEnqueue( QueueFIFO* q, int x )
+{
+
+}
+
+
+
+//=======================================
+int QFDequeue( QueueFIFO* q )
+{
+	return 0;
+}
+
+
+
+//=======================================
+void QFClear( QueueFIFO* q )
+{
+
+}
+
+
+
+//=======================================
+void QFRemove( QueueFIFO* q )
+{
+
+}
+
+
+
+//=======================================
+void QFDel( QueueFIFO* q )
+{
+
+}
